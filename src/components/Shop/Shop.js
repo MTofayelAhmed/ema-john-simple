125b658370a911c5a35198d338b691f441995eb9
MTofayelAhmed <113205143+MTofayelAhmed@users.noreply.github.com>
2022-11-07 22:37:07 +0600

    ema-john

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import data from '../../fakeData/products.JSON';
+
+const Shop = () => {
+ console.log(data)
+   
+  return (
+    <div>
+      <h1>this is Shop</h1>
+
+    </div>
+  );
+};
+
+export default Shop;
